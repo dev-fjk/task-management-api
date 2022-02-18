0c62f9bebd21cda7e5304ac3765d6ec50513e708
dev-fjk <fjk.develop@gmail.com>
2022-02-18 20:21:46 +0900

    DDL/DMLの作成

--- docker/mysql/initdb.d/1_schema.sql
+++ docker/mysql/initdb.d/1_schema.sql
@@ -7,8 +7,67 @@ CREATE TABLE task_user
     mail_address varchar(256) not null comment 'メールアドレス',
     password     varchar(64)  not null comment 'sha256でハッシュ化されたパスワード',
     created_at   timestamp    not null default current_timestamp comment '作成日時',
-    created_src  varchar(30)  not null comment '作成者',
+    created_by   varchar(30)  not null comment '作成者',
     updated_at   timestamp    not null default current_timestamp comment '更新日時',
-    updated_src  varchar(30)  not null comment '更新者',
+    updated_by   varchar(30)  not null comment '更新者',
     PRIMARY KEY (user_id)
 );
+
+create index idx_task_user_name on task_user (last_name, first_name);
+create index idx_task_user_auth on task_user (mail_address, password);
+
+-- ステータスマスタ
+CREATE TABLE task_status_master
+(
+    status_id  INTEGER     not null comment 'ステータスID',
+    status     varchar(20) not null comment 'ステータス',
+    created_at timestamp   not null default current_timestamp comment '作成日時',
+    created_by varchar(30) not null comment '作成者',
+    updated_at timestamp   not null default current_timestamp comment '更新日時',
+    updated_by varchar(30) not null comment '更新者',
+    PRIMARY KEY (status_id)
+);
+
+create index idx_task_status_master_status on task_status_master (status_id, status);
+
+-- 優先度マスタ
+CREATE TABLE task_priority_master
+(
+    priority_id INTEGER     not null comment '優先度ID',
+    priority    varchar(20) not null comment '優先度',
+    created_at  timestamp   not null default current_timestamp comment '作成日時',
+    created_by  varchar(30) not null comment '作成者',
+    updated_at  timestamp   not null default current_timestamp comment '更新日時',
+    updated_by  varchar(30) not null comment '更新者',
+    PRIMARY KEY (priority_id)
+);
+
+create index idx_task_priority_master_priority on task_priority_master (priority_id, priority);
+
+-- タスクテーブル
+CREATE TABLE task
+(
+    task_id     bigint      not null auto_increment comment 'タスクID',
+    user_id     bigint      not null comment 'ユーザーID',
+    status_id   INTEGER     not null default 1 comment 'ステータスID',
+    priority_id INTEGER     not null default 11 comment '優先度ID',
+    start_date  date                 default null comment '開始日',
+    end_date    date                 default null comment '終了日',
+    term_date   date                 default null comment '期限日',
+    created_at  timestamp   not null default current_timestamp comment '作成日時',
+    created_by  varchar(30) not null comment '作成者',
+    updated_at  timestamp   not null default current_timestamp comment '更新日時',
+    updated_by  varchar(30) not null comment '更新者',
+    deleted_at  timestamp            default null comment '削除日時',
+    deleted_by  varchar(30)          default null comment '削除者',
+    is_deleted  tinyint(1)  not null default 0 comment '削除フラグ',
+    PRIMARY KEY (task_id),
+    FOREIGN KEY (user_id) REFERENCES task_user (user_id),
+    FOREIGN KEY (status_id) REFERENCES task_status_master (status_id),
+    FOREIGN KEY (priority_id) REFERENCES task_priority_master (priority_id)
+);
+
+create index idx_task_user_id on task (user_id, is_deleted);
+create index idx_task_user_status_id on task (user_id, status_id, is_deleted);
+create index idx_task_user_priority_id on task (user_id, priority_id, is_deleted);
+create index idx_task_user_term on task (user_id, term_date, is_deleted);
--- docker/mysql/initdb.d/2_initdata.sql
+++ docker/mysql/initdb.d/2_initdata.sql
@@ -1,2 +1,67 @@
-INSERT INTO task_user(last_name, first_name, mail_address,password,created_src,updated_src) VALUES ('Tanaka', 'Tarou','aaa@gmail.com','password','testUser','test_user');
-INSERT INTO task_user(last_name, first_name, mail_address,password,created_src,updated_src) VALUES ('Satou', 'Kazuya','bbb@gmail.com','password2','testUser','test_user');
+-- ユーザー
+INSERT INTO
+    task_user(last_name, first_name, mail_address, password, created_by, updated_by)
+VALUES
+    ('Tanaka', 'Tarou', 'aaa@gmail.com', 'password', 'testUser', 'test_user');
+INSERT INTO
+    task_user(last_name, first_name, mail_address, password, created_by, updated_by)
+VALUES
+    ('Satou', 'Kazuya', 'bbb@gmail.com', 'password2', 'testUser', 'test_user');
+
+-- ステータス マスタ
+INSERT INTO
+    task_status_master(status_id, status, created_by, updated_by)
+VALUES
+    (1, 'TODO', 'admin', 'admin');
+INSERT INTO
+    task_status_master(status_id, status, created_by, updated_by)
+VALUES
+    (2, 'Progress', 'admin', 'admin');
+INSERT INTO
+    task_status_master(status_id, status, created_by, updated_by)
+VALUES
+    (3, 'Wait', 'admin', 'admin');
+INSERT INTO
+    task_status_master(status_id, status, created_by, updated_by)
+VALUES
+    (4, 'Resolved', 'admin', 'admin');
+
+-- 優先度 マスタ
+INSERT INTO
+    task_priority_master(priority_id, priority, created_by, updated_by)
+VALUES
+    (11, 'LOW', 'admin', 'admin');
+INSERT INTO
+    task_priority_master(priority_id, priority, created_by, updated_by)
+VALUES
+    (12, 'MIDDLE', 'admin', 'admin');
+INSERT INTO
+    task_priority_master(priority_id, priority, created_by, updated_by)
+VALUES
+    (13, 'HIGH', 'admin', 'admin');
+
+INSERT INTO
+    task(user_id, status_id, priority_id, start_date, end_date, term_date, created_by, updated_by)
+VALUES
+    (1, 1, 11, null, null, '2022-02-22', '1', '1');
+INSERT INTO
+    task(user_id, status_id, priority_id, start_date, end_date, term_date, created_by, updated_by)
+VALUES
+    (1, 2, 12, '2022-02-15', null, '2022-02-25', '1', '1');
+INSERT INTO
+    task(user_id, status_id, priority_id, start_date, end_date, term_date, created_by, updated_by)
+VALUES
+    (1, 3, 11, '2022-02-20', null, '2022-02-26', '1', '1');
+INSERT INTO
+    task(user_id, status_id, priority_id, start_date, end_date, term_date, created_by, updated_by)
+VALUES
+    (1, 4, 13, '2022-03-01', '2022-03-10', '2022-03-10', '1', '1');
+INSERT INTO
+    task(user_id, status_id, priority_id, start_date, end_date, term_date, created_by, updated_by)
+VALUES
+    (2, 1, 11, null, null, '2022-02-25', '2', '2');
+INSERT INTO
+    task(user_id, status_id, priority_id, start_date, end_date, term_date, created_by, updated_by)
+VALUES
+    (2, 1, 13, null, null, '2022-02-27', '2', '2');
+
