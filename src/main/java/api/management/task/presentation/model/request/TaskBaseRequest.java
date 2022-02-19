fcf2520d4fef3e74a2bc3931af3893e05715024a
dev-fjk <fjk.develop@gmail.com>
2022-02-19 21:35:26 +0900

    相関チェックの追加

--- src/main/java/api/management/task/presentation/model/request/TaskBaseRequest.java
+++ src/main/java/api/management/task/presentation/model/request/TaskBaseRequest.java
@@ -1,7 +1,10 @@
 package api.management.task.presentation.model.request;
 
+import api.management.task.domain.model.consts.TaskPriority;
+import api.management.task.domain.model.consts.TaskStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -9,11 +12,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Data
 public abstract class TaskBaseRequest {
 
-    @NotNull
+    @NotNull(message = "必須項目です")
     @Schema(description = "ステータスID", example = "1", required = true)
     private Integer statusId;
 
-    @NotNull
+    @NotNull(message = "必須項目です")
     @Schema(description = "優先度ID", example = "11", required = true)
     private Integer priorityId;
 
@@ -28,4 +31,14 @@ public abstract class TaskBaseRequest {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @Schema(description = "期限日", example = "2022-02-25")
     private LocalDate termDate;
+
+    @AssertTrue(message = "{validation.status-enabled}")
+    private boolean isEnableStatusId() {
+        return TaskStatus.getIdList().contains(this.statusId);
+    }
+
+    @AssertTrue(message = "{validation.priority-enabled}")
+    private boolean isEnableTaskPriority() {
+        return TaskPriority.getIdList().contains(this.priorityId);
+    }
 }
