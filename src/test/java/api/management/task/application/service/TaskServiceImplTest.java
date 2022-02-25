1f236b362bd41e0823abe39395e9c1ae96434ab6
dev-fjk <fjk.develop@gmail.com>
2022-02-26 00:44:05 +0900

    サンプル追加

--- src/test/java/api/management/task/application/service/TaskServiceImplTest.java
+++ src/test/java/api/management/task/application/service/TaskServiceImplTest.java
@@ -2,7 +2,10 @@ package api.management.task.application.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import api.management.task.domain.model.result.TaskResult;
 import api.management.task.domain.model.result.TaskResultList;
 import api.management.task.domain.model.task.TaskListSelector;
 import api.management.task.domain.repository.TaskRepository;
@@ -33,12 +36,35 @@ class TaskServiceImplTest {
     @Test
     @DisplayName("ユーザーのタスク取得_穴あけ")
     public void 正常系_fetchUserTask() {
+        // given
+        long userId = 10L;
+        long taskId = 100L;
+
+        // mocks
+        var taskResult = TaskResult.builder().build();
+        doReturn(taskResult).when(taskRepository).fetchUserTask(userId, taskId);
+
+        // when-then
+        var expected = target.fetchUserTask(userId, taskId);
+        assertEquals(taskResult, expected);
+        verify(taskRepository, times(1)).fetchUserTask(userId, taskId);
+    }
+
+    @Test
+    @DisplayName("ユーザーのタスク一覧取得_穴あけ")
+    public void 正常系_fetchUserTaskList() {
+        // given
         TaskListSelector selector = TaskListSelector.builder().build();
         int offSet = 1;
         int limit = 20;
 
+        // mocks
         TaskResultList taskResultList = TaskResultList.empty();
         doReturn(taskResultList).when(taskRepository).fetchUserTaskList(selector, offSet, limit);
-        assertEquals(taskResultList, target.fetchUserTaskList(selector, offSet, limit));
+
+        // when-then
+        var expected = target.fetchUserTaskList(selector, offSet, limit);
+        assertEquals(taskResultList, expected);
+        verify(taskRepository, times(1)).fetchUserTaskList(selector, offSet, limit);
     }
 }
